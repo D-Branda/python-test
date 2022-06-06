2d02a4c54f42a1f4156d0fc23315ffb24eab940d
dpjb-it <DomingosBranda@outlook.com>
2022-06-06 19:13:12 +0100

    second commit. commentaries added.

--- python_test.py
+++ python_test.py
@@ -1,14 +1,19 @@
+# commentary added for printmsg
 def printmsg:
   print(f"Welcome to github")
 
+# commentary added for add
 def add(a, b):
 	pass
 
+# commentary added for sub
 def sub(a, b):
 	pass
 
+# commen added for mult
 def mult(a, b):
 	return a*b
 
+# commentary added for div
 def div(a, b):
 	pass
